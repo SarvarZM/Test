18eb4c8b9681b7eeaf489e2b0fcf594c2866835f
SarvarZM <sarvar.muratov70@gmail.com>
2024-06-13 19:18:04 -0500

    JavaProject

---
+++ src/collections/Intro.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Intro {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(6);
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(7);
+        stack.push(5);
+
+        Stack<String> words= new Stack<>();
+
+
+        System.out.println(stack.pop());
+
+
+
+    }
+}
